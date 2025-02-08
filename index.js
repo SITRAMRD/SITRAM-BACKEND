483e06580273d17b251d269986be27395a1459cf
Daviel Alexander Sanchez <Davielalexsanchez@gmail.com>
2025-02-08 01:50:27 -0400

    Revert "Configurando el Swagger para documentar la API."
    This reverts commit bb7053a8698026da5badb091b7903a4b151f0e3f.

--- index.js
+++ index.js
@@ -1,47 +1,12 @@
 const express = require("express");
 const dotenv = require('dotenv')
-const swaggerUi = require('swagger-ui-express')
-const specs = require('./swagger/swagger')
-const cors = require('cors')
-
-dotenv.config();
 const app = express();
 
+dotenv.config();
 app.use(express.json());
 
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(specs))
 
-app.use(cors({
-    origin: ['*'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-}));
-
-/**
- * @swagger
- * tags:
- *   name: Principal | Bienvenida
- */
-
-/**
- * @openapi
- * /api:
- *   get:
- *     summary: EndPoint principal de la API.
- *     description: Bienvenido a la API de SITRAMRD!
- *     tags: [Principal | Bienvenida]
- *     responses:
- *       200:
- *         description: Retorna un mensaje de aprobacion por Ok.
- *         content:
- *             application/json:
- *                  schema:
- *                      type: string
- *       400: 
- *         description: Retorna un mensaje de error por Bad Request.
- *       404:
- *         description: Retorna un mensaje de error por Not Found.
- */
-app.get("/api", (req, res) => {
+app.get("/", (req, res) => {
     res.json({ mensaje: "¡Bienvenido a la API de SITRAMRD!" });
 });
 
