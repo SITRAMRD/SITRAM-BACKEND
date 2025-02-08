6b81fdc4c9d20b48c2902a8c9c8b0501c473dd7f
Daviel Alexander Sanchez <Davielalexsanchez@gmail.com>
2025-02-08 01:54:51 -0400

    Configurando el Swagger para documentar la API.

--- index.js
+++ index.js
@@ -1,12 +1,47 @@
 const express = require("express");
 const dotenv = require('dotenv')
-const app = express();
+const swaggerUi = require('swagger-ui-express')
+const specs = require('./swagger/swagger')
+const cors = require('cors')
 
 dotenv.config();
+const app = express();
+
 app.use(express.json());
 
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(specs))
 
-app.get("/", (req, res) => {
+app.use(cors({
+    origin: ['*'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+}));
+
+/**
+ * @swagger
+ * tags:
+ *   name: Principal | Bienvenida
+ */
+
+/**
+ * @openapi
+ * /api:
+ *   get:
+ *     summary: EndPoint principal de la API.
+ *     description: Bienvenido a la API de SITRAMRD!
+ *     tags: [Principal | Bienvenida]
+ *     responses:
+ *       200:
+ *         description: Retorna un mensaje de aprobacion por Ok.
+ *         content:
+ *             application/json:
+ *                  schema:
+ *                      type: string
+ *       400: 
+ *         description: Retorna un mensaje de error por Bad Request.
+ *       404:
+ *         description: Retorna un mensaje de error por Not Found.
+ */
+app.get("/api", (req, res) => {
     res.json({ mensaje: "¡Bienvenido a la API de SITRAMRD!" });
 });
 
